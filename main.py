faaa17b2eda2cc3aa40b8bec583bb39c3ec3b301
zh-serikov <zhasulanserikov96@gmail.com>
2019-02-20 18:18:37 +0600

    Fix words with punctuation, html entities; render whole layout

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import html
 import re
-import string
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, urlunparse
 
 import requests
 from bs4 import BeautifulSoup, NavigableString
 
-
-# count punctuation in string/word (Ex: 'blog,.' has 2 punc )
-def count_punc(s, punc):
-    return sum([1 for x in s if x in punc])
+# modified version of string punctuation
+punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~«»"""
 
 
 # adding TRADE SYMBOL for 6 length word(without punctuation)
 def modify_words(array):
+    remove_punc = str.maketrans(punctuation, ' ' * len(punctuation))
     for i, word in enumerate(array):
-        word = word.strip()
-        c = count_punc(word, set(string.punctuation))
-        if len(word) - c == 6:
-            array[i] = word + html.unescape('&#8482;')
+        without_punc = word.translate(remove_punc)
+        myarr = re.split(r'(\s+)', without_punc)
+        for j, part in enumerate(myarr):
+            if len(part) == 6:
+                word = word.replace(part, part + html.unescape('&#8482;'))
+
+        array[i] = word
 
     return ''.join(array)
 
@@ -28,34 +29,29 @@ def modify_words(array):
 def change_in_tag(root):
     for desc in list(root.descendants):
         if isinstance(desc, NavigableString):
-            if desc.strip() != '':
-                arr = re.split(r'(\s+)', desc)  # re.split saves whitespaces as original
-                newchild = modify_words(arr)
-                desc.replace_with(newchild)
+            arr = re.split(r'(\s+)', desc)  # re.split saves whitespaces as original
+            newchild = modify_words(arr)
+            desc.replace_with(newchild)
 
 
-def get_soup_from_request(url, self):
+def modify_content_from_url(url, self):
     r = requests.get(url)
 
     if r.status_code == 200 and ("text/html" in r.headers["content-type"]):
 
-        soup = BeautifulSoup(r.text, 'html.parser')
+        soup = BeautifulSoup(html.unescape(r.text), 'html.parser')
 
-        # replace all habr links with localhost
+        # replace all habr links with localhost domain
         for link in soup.find_all('a', attrs={'href': re.compile("habrahabr.ru|habr.com")}):
             scheme, netloc, path, params, query, fragment = urlparse(link['href'])
             new_url = urlunparse(('http', 'localhost:3000', path, params, query, fragment))
             link['href'] = new_url
+        with open("/home/zhasulan/Downloads/sometxt.txt", 'w') as f:
+            f.write(html.unescape(r.text))
 
-        # for habr post list
-        posts_lists = soup.find('div', attrs={'class': 'posts_lists'})
-        if posts_lists:
-            change_in_tag(posts_lists)
-
-        # for habr full post view
-        post_body = soup.find('article')
-        if post_body:
-            change_in_tag(post_body)
+        layout = soup.find('div', attrs={'class': 'layout'})
+        if layout:
+            change_in_tag(layout)
 
         self.wfile.write(bytes(str(soup), "utf-8"))
 
@@ -74,12 +70,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.set_headers()
         if self.path == '/':
-            get_soup_from_request("https://habr.com", self)
+            modify_content_from_url("https://habr.com", self)
 
         if self.path != '/':
             real_url = "https://habr.com" + self.path
-            print(self.path)
-            get_soup_from_request(real_url, self)
+            modify_content_from_url(real_url, self)
 
 
 def run():
